923fa46c9c22efcbf08363529407b0624411e357
Hoani-TeWhata <78768304+Hoani-TeWhata@users.noreply.github.com>
2021-02-16 13:16:53 +1300

    First Upload MMF Base file
    This my first upload for the MMF base file

---
+++ 00_MMF_base.py
@@ -0,0 +1,32 @@
+# import statemants
+
+
+# functions go here
+
+
+# ^^^^^^^^^^^^^^^^ Main Routine ^^^^^^^^^^^^^^^^^^
+
+# Set up dictionaries / lists needed to hold data
+
+# Ask user if they have used the program & show instructions if necessary
+
+# Loop to get ticket details
+
+     # Get name (can't be blank)
+
+     # Get age (between 12 and 130)
+
+     # Calculate ticket price
+
+     # Loop to ask for snacks
+
+     # Calculate snack price
+
+     # ask for payments methid (and apply surcharge if necessary)
+
+
+# Calculate Total sales and profit
+
+# Output data to text file
+
+
